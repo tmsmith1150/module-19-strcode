3cadf76bab18eb55d44f169c47a28cf183e7207f
Michelle Jordan <tmsmith1150@live.com>
2022-10-19 15:23:10 -0400

    deploy

---
+++ client/src/js/database.js
@@ -0,0 +1,53 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('PUT to database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  // Use the .put method to update the data in the database
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database and version we want to use
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store
+  const store = tx.objectStore('jate');
+
+  // Use the .getall() method to get all data in the database
+  const request = store.getAll();
+
+  // Get confirmation of the request
+  const result = await request;
+  result
+      ? console.log('Data retrieved from database')
+      : console.log('Data not found in the database.');
+  return result?.value;
+};
+
+initdb();
